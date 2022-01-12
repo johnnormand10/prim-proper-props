e224a736924222ac561756ceacc6ff9c34c39d1e
Johnathan Normand <jnormand10@gmail.com>
2022-01-12 15:16:52 -0600

    moved <footer>, <h3>, and <p> tags into the footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer(props){
+    return(
+        <footer>
+            <h3>
+                {props.h3}
+            </h3>
+            <p>
+                {props.p}
+            </p>
+        </footer>
+    );
+}
+
+export default Footer;
