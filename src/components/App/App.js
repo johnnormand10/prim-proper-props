f75d82ea8e285f4a60711ddb7e563b9da742b27a
Johnathan Normand <jnormand10@gmail.com>
2022-01-12 16:27:41 -0600

    moved the form into GuestForm component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,10 +14,12 @@ import GuestList from '../GuestList/GuestList';
 //importing DinnerSupplies
 import DinnerSupplies from '../DinnerSupplies/DinnerSupplies';
 
+//importing GuestForm
+import GuestForm from '../GuestForm/GuestForm';
 function App() {
   let [guestList, setGuestList] = useState([]);
-  let [newGuestName, setNewGuestName] = useState('');
-  let [newGuestMeal, setNewGuestMeal] = useState('false');
+  // let [newGuestName, setNewGuestName] = useState('');
+  // let [newGuestMeal, setNewGuestMeal] = useState('false');
 
   //On load, get guests
   useEffect(() => {
@@ -36,12 +38,12 @@ function App() {
   }
 
 
-  const addGuest = () => {
-    axios.post('/guests', { name: newGuestName, kidsMeal: newGuestMeal })
+  const addGuest = (newGuest) => {
+    axios.post('/guests', newGuest)
       .then(response => {
         // clear inputs
-        setNewGuestName('');
-        setNewGuestMeal(false);
+        // setNewGuestName('');
+        // setNewGuestMeal(false);
 
         getGuests();
       })
@@ -52,68 +54,19 @@ function App() {
   };
 
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (newGuestName) {
-      addGuest();
-    }
-    else {
-      alert('The new guest needs a name!');
-    }
-  }
-
-  console.log(newGuestMeal)
+  //console.log(newGuestMeal)
   return (
     <div className="App">
 
       <Header title = "Prim Proper Props"/>
       <h2>Party Leader</h2>
       {guestList[0] && <h3>{guestList[0].name}</h3>}
-      <h2>Add a new guest</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name
-        </label>
-        <input
-          type="text"
-          placeholder="Name"
-          value={newGuestName}
-          onChange={(evt) => setNewGuestName(evt.target.value)}
-        />
-        <div>
-          Would this guest like a kid's meal?
-          <div >
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  value={true}
-                  checked={newGuestMeal === 'true'}
-                  name="kidsMeal"
-                  onChange={(evt) => setNewGuestMeal(evt.target.value)}
-                />
-                Yes, this guest would like a Kid's Meal
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  value={false}
-                  checked={newGuestMeal === 'false'}
-                  name="kidsMeal"
-                  onChange={(evt) => setNewGuestMeal(evt.target.value)}
-                />
-                No, this guest would not like a Kid's Meal
-              </label>
-            </div>
-          </div>
-        </div>
-        <button type="submit">Add Guest</button>
-      </form>
+      <GuestForm 
+        addGuest = {addGuest}
+      />
       <GuestList guestList = {guestList}/>
-      <DinnerSupplies supplies = {guestList}/>
-      <Footer h3 = "Have Fun!" p = "Don't forget your Ps and Qs!"/>
+      <DinnerSupplies guestList = {guestList}/>
+      <Footer />
     </div>
   );
 }
--- src/components/DinnerSupplies/DinnerSupplies.js
+++ src/components/DinnerSupplies/DinnerSupplies.js
@@ -1,15 +1,15 @@
-function DinnerSupplies({supplies}){
+function DinnerSupplies({guestList}){
     return(
         <>
         <h2>Dinner Supplies</h2>
         <div>
-        Spoons: {supplies.length * 2}
+        Spoons: {guestList.length * 2}
         </div>
         <div>
-        Forks: {supplies.length * 2}
+        Forks: {guestList.length * 2}
         </div>
         <div>
-        Knives: {supplies.length * 2}
+        Knives: {guestList.length * 2}
         </div>
         </>
     );
